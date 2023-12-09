ace0910b4a38e8acf1da318d3356540e363ecf0d
Hanna Negash <hnegash.n@gmail.com>
2023-12-09 14:01:53 -0800

    Add a comma

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,20 @@
 
 def greet_programmer():
     print("Hello, programmer!")
+greet_programmer()
 
 def greet(name):
     print(f"Hello, {name}!")
+greet("Naureen")
 
 def greet_with_default(name="programmer"):
-    print(f"Hello {name}")
+    print(f"Hello, {name}")
+greet_with_default("Sunny")
 
 def add(num1, num2):
     return num1 + num2
+add(1, 2)
 
 def halve(number):
     return number / 2
+halve(4)
